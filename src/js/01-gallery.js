3e8225afecc876b9fa76541b7befdcbf022d7c04
MaxKov86 <max.kov86@ukr.net>
2023-01-23 13:19:12 +0300

    Update JS and HTML

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+galleryList.addEventListener('click', onImgClick);
+
+galleryList.insertAdjacentHTML('beforeend', renderGalleryList(galleryItems));
+
+function renderGalleryList(gallery) {
+  return gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    )
+    .join('');
+}
+
+function onImgClick(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+      
+       <img src=${e.target.dataset.source}>
+
+  `);
+
+  instance.show();
+
+  document.body.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
