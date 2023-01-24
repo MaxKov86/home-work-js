80396b3247811c1aae9a5d4a22d3f50ea56a99db
MaxKov86 <max.kov86@ukr.net>
2023-01-24 13:16:48 +0300

    Update JS and HTML

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+galleryList.insertAdjacentHTML('beforeend', renderCardList(galleryItems));
+
+function renderCardList(gallery) {
+  return gallery.map(
+    ({ original, preview, description }) => `<li>
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+  );
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+document.body.style.backgroundColor = 'grey';
